d4d2167b0b233d8bb42bf14df7e333b48487f88c
mrousavy-tgm <mrousavy-tgm@users.noreply.github.com>
2017-10-13 19:31:47 +0200

    Implement paste with  Text and File

--- pastebin.sh
+++ pastebin.sh
@@ -1,14 +1,22 @@
+title='QPaste - http://github.com/mrousavy/QPastebin'
 expiration='1H' # N = Never | 10M = 10 Minutes | 1H = 1 Hour ..
-token='' # Pastebin API Token (Login with account and get token on pastebin.com/api)
-input='test' # The actual input - via command args
+token='4779df04c88bd717d6a7afe671bf7375' # Pastebin API Token (Login with account and get token on pastebin.com/api)
 
-echo $token
+if [ $# -eq 0 ]
+then
+	echo 'No arguments specified!'
+	echo 'Usage: qpaste "MY TEXT"'
+	echo '     : qpaste myfile.txt'
+else	
+	input=$1 # The actual input - via command args
 
-curl -X POST https://pastebin.com/api/api_post.php -d '
-	api_dev_key=,
-	api_option=paste,
-	api_paste_code=test,
-	api_paste_private=1,
-	api_paste_name=QPaste - http://github.com/mrousavy/QPastebin,
-	api_paste_expire_date=1H'
+	if [ -f $1] # If arg 1 is a file:
+	then
+		input="$(cat $1)" # It's a file -> Read content
+	fi
+
+	query="api_option=paste&api_dev_key=${token}&api_paste_code=${input}&api_paste_private=1&api_paste_name=${title}&api_paste_expire_date=${expiration}"
+	curl -X POST https://pastebin.com/api/api_post.php -d "${query}"
+fi
+
 echo ''
