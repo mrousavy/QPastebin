cc2919727d525af4a7178f2f4994a9ca14fbfbc7
mrousavy-tgm <mrousavy-tgm@users.noreply.github.com>
2017-10-13 19:08:32 +0200

    Update script for parameters

--- pastebin.sh
+++ pastebin.sh
@@ -1,11 +1,16 @@
-expiration="1H"
+expiration='1H' # N = Never | 10M = 10 Minutes | 1H = 1 Hour ..
+token='' # Pastebin API Token (Login with account and get token on pastebin.com/api)
+input='test' # The actual input - via command args
 
-curl -X POST -i -H "Content-type: application/json" -X POST https://pastebin.com/api/api_post.php -d '
+echo $token
+
+curl -X POST https://pastebin.com/api/api_post.php -d '
 {
-	{ "api_dev_key",  $(token) },
-	{ "api_option", "paste" },
-	{ "api_paste_code", $(input) },
-	{ "api_paste_private" , "1" },
-	{ "api_paste_name", "QPaste - http://github.com/mrousavy/QPastebin" },
-	{ "api_paste_expire_date", $(expiration) }
+	"api_dev_key"			:  	$token,
+	"api_option"			:	"paste",
+	"api_paste_code"		: 	$input,
+	"api_paste_private"		:	1,
+	"api_paste_name"		:	"QPaste - http://github.com/mrousavy/QPastebin",
+	"api_paste_expire_date"	:	$expiration
 }'
+echo ''
