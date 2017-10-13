c75d1a240816af9fad37ae3dc4eddcc7e3dd739a
mrousavy-tgm <mrousavy-tgm@users.noreply.github.com>
2017-10-13 18:46:10 +0200

    Adjust indentation

--- pastebin.sh
+++ pastebin.sh
@@ -1,9 +1,9 @@
 curl -X POST -i -H "Content-type: application/json" -X POST https://pastebin.com/api/api_post.php -d '
-    {
+{
 		{ "api_dev_key",  $token },
 		{ "api_option", "paste" },
 		{ "api_paste_code", $input },
 		{ "api_paste_private" , "1" },
 		{ "api_paste_name", "QPaste - http://github.com/mrousavy/QPastebin" },
 		{ "api_paste_expire_date", "1H" }
-	}'
+}'
